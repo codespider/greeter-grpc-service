7cdcb55184b7d7d67d3556cafa9ad75c992ec54a
karthik <karthikkannan@gmail.com>
2019-04-29 15:30:13 +0530

    Adding consul registration

--- register.py
+++ register.py
@@ -1,10 +1,7 @@
 import logging
 import os
+from consul import Consul
 
-import requests
-from greeter_grpc import greeter
-
-PORT = os.environ.get('GREETING_SERVICE_PORT', 50051)
 CONSUL_REGISTRATION_ENABLED = os.environ.get('CONSUL_REGISTRATION_ENABLED', 'FALSE')
 CONSUL_SERVICE_NAME = os.environ.get('CONSUL_SERVICE_NAME', 'greeter')
 CONSUL_AGENT_HOST = os.environ.get('CONSUL_AGENT_HOST', '127.0.0.1')
@@ -12,8 +9,7 @@ CONSUL_AGENT_PORT = os.environ.get('CONSUL_AGENT_PORT', 8500)
 
 if __name__ == '__main__':
     logging.basicConfig()
-    greeter.serve(PORT)
-    try:
-        requests.put(f"http://{CONSUL_AGENT_HOST}:8500/v1/agent/service/register", json={"Name": "greet-curl"})
-    except:
-        print("Error")
+    if CONSUL_REGISTRATION_ENABLED == 'TRUE':
+        print(f"registering with consul agent {CONSUL_AGENT_HOST}:8500")
+        consul = Consul(host=CONSUL_AGENT_HOST, port=8500)
+        consul.agent.service.register(CONSUL_SERVICE_NAME)
